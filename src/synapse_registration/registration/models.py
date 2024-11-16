da40dbef8522bd2a380ada06a2caf01db07c512e
Kumi <git@kumi.email>
2024-11-16 21:21:31 +0100

    feat: Add timestamp to user registration model
    Introduces a timestamp field to the UserRegistration model to
    automatically capture the creation time of each entry. This helps
    track and manage registration history more effectively.

--- src/synapse_registration/registration/models.py
+++ src/synapse_registration/registration/models.py
@@ -23,6 +23,7 @@ class UserRegistration(models.Model):
     status = models.IntegerField(choices=STATUS_CHOICES, default=STATUS_STARTED)
     token = models.CharField(max_length=64, unique=True)
     email_verified = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.username
