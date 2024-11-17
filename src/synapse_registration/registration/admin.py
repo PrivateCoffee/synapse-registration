3a032b02f4cfb02c17cc112468d8a72f2be34c9b
Kumi <git@kumi.email>
2024-11-17 11:52:35 +0100

    fix: Enhances admin site UI headers
    Updates admin interface to include a site title and
    index title for better user guidance and branding.

--- src/synapse_registration/registration/admin.py
+++ src/synapse_registration/registration/admin.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import Group
 from .models import UserRegistration
 
 admin.site.site_header = "Synapse Registration Administration"
+admin.site.site_title = "Synapse Registration Administration"
+admin.site.index_title = "Welcome to the Synapse Registration Administration"
 
 admin.site.unregister(Group)
 
