c5adcabd17c3f7dd82c199136878dba615e96927
Kumi <git@kumi.email>
2025-12-05 10:28:54 +0100

    fix: Correct module path in asgi

--- src/synapse_registration/synapse_registration/asgi.py
+++ src/synapse_registration/synapse_registration/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'synapse_registration.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "synapse_registration.synapse_registration.settings"
+)
 
 application = get_asgi_application()
